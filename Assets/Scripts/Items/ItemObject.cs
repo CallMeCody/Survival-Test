5ff09d4ea177c20fab3a7d1dc34f229f832d674d
Cody Carpenter <cody_carpenter2222@yahoo.com>
2021-04-13 22:52:02 -0700

    Items are adding to inventory, stacking works, and items drop when trying to pick up an item with a full inventory

--- Assets/Scripts/Items/ItemObject.cs
+++ Assets/Scripts/Items/ItemObject.cs
@@ -6,13 +6,16 @@ public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData item;
 
+    // returns the prompt we want to show on-screen when hovering over the item
     public string GetInteractPrompt()
     {
         return string.Format("Pickup {0}", item.displayName);
     }
 
+    // called when we interact with the item
     public void OnInteract()
     {
+        Inventory.instance.AddItem(item);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -43,6 +43,155 @@ public class Inventory : MonoBehaviour
         instance = this;
         controller = GetComponent<PlayerController>();
     }
+
+    void Start()
+    {
+        inventoryWindow.SetActive(false);
+        slots = new ItemSlot[uiSlots.Length];
+
+        // initialize the slots
+        for(int x = 0; x < slots.Length; x++)
+        {
+            slots[x] = new ItemSlot();
+            uiSlots[x].index = x;
+            uiSlots[x].Clear();
+        }
+    }
+
+    void Toggle()
+    {
+
+    }
+
+    public bool IsOpen()
+    {
+        return inventoryWindow.activeInHierarchy;
+    }
+
+    // adds the requested item to the player's inventory
+    public void AddItem(ItemData item)
+    {
+        // does this item have a stack it can be added to?
+        if (item.canStack)
+        {
+            ItemSlot slotToStackTo = GetItemStack(item);
+
+            if(slotToStackTo != null)
+            {
+                slotToStackTo.quantity++;
+                UpdateUI();
+                return;
+            }
+        }
+
+        ItemSlot emptySlot = GetEmptySlot();
+
+        // do we have an empty slot for the item?
+        if(emptySlot != null)
+        {
+            emptySlot.item = item;
+            emptySlot.quantity = 1;
+            UpdateUI();
+            return;
+        }
+
+        // if the item can't stack and there are no empty slots - throw it away
+        ThrowItem(item);
+    }
+
+    // spawns the item infront of the player
+    void ThrowItem (ItemData item)
+    {
+        Instantiate(item.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360.0f));
+    }
+
+    // updates the UI slots
+    void UpdateUI()
+    {
+        for(int x = 0; x < slots.Length; x++)
+        {
+            if (slots[x].item != null)
+                uiSlots[x].Set(slots[x]);
+            else
+                uiSlots[x].Clear();
+        }
+    }
+
+    //returns the item slot that the requested item can be stacked on
+    // returns null if there is no stack available
+    ItemSlot GetItemStack(ItemData item)
+    {
+        for(int x = 0; x < slots.Length; x++)
+        {
+            if (slots[x].item == item && slots[x].quantity < item.maxStackAmount)
+                return slots[x];
+        }
+
+        return null;
+    }
+
+    // returns an empty slot in the inventory
+    // if there are not empty slot - return null
+    ItemSlot GetEmptySlot()
+    {
+        for (int x = 0; x < slots.Length; x++)
+        {
+            if (slots[x].item == null)
+                return slots[x];
+        }
+
+        return null;
+    }
+
+    public void SelectItem(int index)
+    {
+
+    }
+
+    void ClearSelectedItemWindow()
+    {
+
+    }
+
+    public void OnUseButton()
+    {
+
+    }
+
+    public void OnEquipButton()
+    {
+
+    }
+
+    void UnEquip(int index)
+    {
+
+    }
+
+    public void OnUnEquipButton()
+    {
+
+    }
+
+    public void OnDropButton()
+    {
+
+    }
+
+    void RemoveSelectedItem()
+    {
+
+    }
+
+    public void RemoveItem (ItemData item)
+    {
+
+    }
+
+    public bool HasItems(ItemData item, int quantity)
+    {
+        return false;
+    }
 }
 
 public class ItemSlot
